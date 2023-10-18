f6fa84108c6bde1878317a888b4ea8083881c63c
Slavkovikj5612 <148367717+Slavkovikj5612@users.noreply.github.com>
2023-10-18 18:39:41 +0200

    Update luka.java

--- lecture3/luka.java
+++ lecture3/luka.java
@@ -1,1 +1,28 @@
+package myNumber;
 
+public class myNumberr {
+	private int num;
+	public int num2;
+	public myNumberr() {
+		num=0 ;
+		num2=100;
+	}
+	int getNum() {
+		return num;
+	}
+	void SetNum(int i) {
+		num=i;
+		num2=10*i;
+	}
+
+
+public class myclass {
+		public static void main (String[]ar) {
+			myNumberr A=new myNumberr();
+			System.out.println(A.getNum());
+			A.SetNum(10);
+			System.out.println(A.getNum());
+			System.out.println(A.num2);
+		}
+}
+}
